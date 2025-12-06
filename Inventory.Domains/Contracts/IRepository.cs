19a617c46e3461bc0360d0c81d2d1ce2e947fca2
AhmedWael <ahmed.wael.2119@gmail.com>
2025-12-06 17:02:39 +0200

    Create Base Entity to be Base Of any Repository Create  (Product DM /Product Service) with Unit of work Repo and Generic repositoy

---
+++ Inventory.Domains/Contracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Inventory.Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domains.Contracts
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ Inventory.Persistence/Data/Context/InventoryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Persistence.Data.Context
+{
+    public class InventoryDbContext(DbContextOptions<InventoryDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(InventoryDbContext).Assembly);
+        }
+
+    }
+}
